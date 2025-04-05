9a73270998003940c2f743e2d68aff395e0898cb
Lara054 <katutosi0504@icloud.com>
2025-04-05 12:28:10 +0900

    施設紹介のslick追加

--- js/script.js
+++ js/script.js
@@ -1,26 +1,33 @@
 $(window).on("load", function () {
-    $(".hero").addClass("show");
+  $('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 
-    $(".fade-item").each(function (i) {
-        const $this = $(this);
-        setTimeout(function () {
-            $this.addClass("show");
-        }, i * 800);
-    });
+  $(".hero").addClass("show");
+
+  $(".fade-item").each(function (i) {
+    const $this = $(this);
+    setTimeout(function () {
+      $this.addClass("show");
+    }, i * 800);
+  });
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("visible");
-          observer.unobserve(entry.target);
-        }
-      });
-    });
-  
-    document.querySelectorAll(".fade-in").forEach((el) => {
-      observer.observe(el);
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
     });
   });
-  
+
+  document.querySelectorAll(".fade-in").forEach((el) => {
+    observer.observe(el);
+  });
+});
+
